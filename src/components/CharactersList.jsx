f81a8451c1b0d150cb7a7617265392279bea2d97
sopadelletres01 <marcportero01@gmail.com>
2022-09-23 21:24:48 +0200

    refactor characterList

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import { useGetCharacters } from "../hooks/useGetCharacters";
 import CharacterCard from "./CharacterCard";
-import { useFavourites } from "../hooks/useFavourites";
 
 function CharactersList() {
   const characters = useGetCharacters();
-  // const { favourites, toggleFavourite } = useFavourites();
-  // const handleToggleFavourite = (char)=>{
-  //   toggleFavourite(char)
-  // }
 
   const renderCharacters = () => {
-    console.log("Tusa")
-    // return characters.results.map((char) => {
-    //   if (favourites.find((fav) => fav.id === char.id)) {
-    //     return <CharacterCard key={char.id} handleToggle={(id)=>handleToggleFavourite(id)} char={char} isFavourite />;
-    //   }
-    //   return <CharacterCard key={char.id} handleToggle={(id)=>handleToggleFavourite(id)} char={char} />;
-    // });
     return characters.results.map((char) => {
       return <CharacterCard key={char.id} char={char} />;
     });
@@ -26,7 +14,7 @@ function CharactersList() {
   return (
     <section>
       <h1>Characters List</h1>
-      <div className="charactersList  flex gap-2 justify-center flex-wrap">
+      <div className="charactersList flex gap-2 justify-center flex-wrap">
         {characters && characters.results && renderCharacters()}
       </div>
     </section>
