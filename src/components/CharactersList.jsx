0922f5671f2ea9f7a90d1095fa5bab5e431c62af
sopadelletres01 <marcportero01@gmail.com>
2022-09-24 01:48:26 +0200

    first aproach of pagination

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useMemo } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useGetCharacters } from "../hooks/useGetCharacters";
 import CharacterCard from "./CharacterCard";
+import Pagination from "./Pagination";
+
+let PageSize = 20;
 
 function CharactersList() {
-  const characters = useGetCharacters();
+  const { characters, info } = useGetCharacters();
+  const [currentTableData, setCurrentTableData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    setCurrentTableData(characters);
+  }, [characters]);
+
+  const handlePageChange = async (page) => {
+    try {
+      setCurrentPage(page);
+      const data = await fetch(
+        import.meta.env.VITE_CHARACTERS_URL + "?page=" + page
+      );
+      const serializedData = await data.json();
+      setCurrentTableData(serializedData.results);
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
 
   const renderCharacters = () => {
-    return characters.results.map((char) => {
+    return currentTableData.map((char) => {
       return <CharacterCard key={char.id} char={char} />;
     });
   };
@@ -14,9 +39,23 @@ function CharactersList() {
   return (
     <section>
       <h1>Characters List</h1>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={info.count || characters.length}
+        pageSize={PageSize}
+        onPageChange={handlePageChange}
+      />
       <div className="charactersList flex gap-2 justify-center flex-wrap">
-        {characters && characters.results && renderCharacters()}
+        {currentTableData && characters && renderCharacters()}
       </div>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={info.count || characters.length}
+        pageSize={PageSize}
+        onPageChange={handlePageChange}
+      />
     </section>
   );
 }
--- src/hooks/useGetCharacters.js
+++ src/hooks/useGetCharacters.js
@@ -1,15 +1,32 @@
 import { useState, useEffect } from "react";
 
 const useGetCharacters = () => {
-  const [characters, setCharacters] = useState();
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState([]);
+
   useEffect(() => {
     (async () => {
       const data = await fetch(import.meta.env.VITE_CHARACTERS_URL);
       const serializedData = await data.json();
-      setCharacters(serializedData);
+      setCharacters(serializedData.results);
+      setInfo(serializedData.info);
     })();
   }, []);
-  return characters;
+  return {characters,info};
 };
 
 export { useGetCharacters };
+
+
+
+// const useGetCharacters = () => {
+//   const [characters, setCharacters] = useState([]);
+//   useEffect(() => {
+//     (async () => {
+//       const data = await fetch(import.meta.env.VITE_CHARACTERS_URL);
+//       const serializedData = await data.json();
+//       setCharacters(serializedData.results);
+//     })();
+//   }, []);
+//   return characters;
+// };
